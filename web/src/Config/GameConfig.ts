3c243ff186bcb3a4a2f82680143a535f94aa88c4
melnikkovva <melnikovama555@gmail.com>
2026-07-09 14:36:02 +0300

    сложные коллизии

---
+++ web/src/Config/GameConfig.ts
@@ -0,0 +1,4 @@
+export const START_SEND_TIME_VALUE = 0;
+export const DEFAULT_SPEED_Y = 0;
+export const MILLISECONDS_IN_SECOND = 1000;
+export const MAX_DELTA_TIME = 0.05;
---
+++ web/src/Config/PlayerConfig.ts
@@ -0,0 +1,5 @@
+import * as THREE from "three";
+
+export const LOCAL_PLAYER_ID = "local_player";
+export const PLAYER_SPAWN_POSITION = new THREE.Vector3(35, 1.2, -9);
+export const PLAYER_COLLIDER_SIZE = new THREE.Vector3(0.6, 1.8, 0.6);
