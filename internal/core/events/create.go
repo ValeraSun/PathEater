655eec41f7af2ebdbec30f226737181e424e84ff
Alexandr <ak2931@yandex.ru>
2026-07-15 17:00:45 +0300

    доделал мувмнт коллизию создание персонажа отдавание камеры (забыл добавить новые файлы)

---
+++ internal/core/events/create.go
@@ -0,0 +1,12 @@
+package events
+
+type CreatePlayerEvent struct {
+	ID string
+}
+
+func (*CreatePlayerEvent) Type() string { return "createPlayer" }
+func NewCreatePlayerEvent(id string) *CreatePlayerEvent {
+	return &CreatePlayerEvent{
+		ID: id,
+	}
+}
---
+++ internal/core/systems/create.go
@@ -0,0 +1,61 @@
+package systems
+
+import (
+	"fmt"
+
+	"github.com/ValeraSun/PathEater/internal/core/entities"
+	"github.com/ValeraSun/PathEater/internal/core/events"
+)
+
+type CreateSystem struct {
+	adder       systemAdder
+	getter      componentsGetter
+	broadcaster Broadcaster
+	eventQueue  chan *events.CreatePlayerEvent
+}
+
+func NewCreateSystem(adder systemAdder, getter componentsGetter, broadcaster Broadcaster, subscriber subscriber) *CreateSystem {
+	s := &CreateSystem{
+		adder:      adder,
+		getter:     getter,
+		eventQueue: make(chan *events.CreatePlayerEvent, 100),
+	}
+	subscriber.Subscribe("create", s.OnEvent)
+	return s
+}
+
+func (s *CreateSystem) Update(dt float32) error {
+
+	s.drainEvents()
+
+	return nil
+}
+
+func (s *CreateSystem) drainEvents() {
+	for {
+		select {
+		case e := <-s.eventQueue:
+			player := entities.NewPlayer(s.adder)
+			s.broadcaster.CreateCameraForPlayer(e.ID, player)
+		default:
+			return
+		}
+	}
+}
+
+func (s *CreateSystem) OnEvent(event events.Event) error {
+	ps, ok := event.(*events.CreatePlayerEvent)
+
+	if !ok {
+		return nil
+	}
+
+	select {
+	case s.eventQueue <- ps:
+
+	default:
+		fmt.Printf("Преполена очередь %v\n", s)
+	}
+
+	return nil
+}
