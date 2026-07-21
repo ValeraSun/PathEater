e87a7b1ca446c3cc7fd8a1afd84e07cc98afe45a
EgorPras <egorprasnikov@gmail.com>
2026-07-21 09:12:13 +0300

    Update cosmoalien.go

--- internal/core/components/cosmoalien.go
+++ internal/core/components/cosmoalien.go
@@ -1,7 +1,7 @@
 package components
 
-type CosmoAlienComponent struct{
-	died bool
+type CosmoAlienComponent struct {
+	Died bool
 }
 
 func (*CosmoAlienComponent) Type() string {
@@ -10,6 +10,6 @@ func (*CosmoAlienComponent) Type() string {
 
 func NewCosmoAlienComponent() *CosmoAlienComponent {
 	return &CosmoAlienComponent{
-		died: false,
+		Died: false,
 	}
 }
