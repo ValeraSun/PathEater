09b93ced3dd296150bfd186292603f335429faeb
EgorPras <egorprasnikov@gmail.com>
2026-07-24 11:44:48 +0300

    Fix

--- internal/core/game/game.go
+++ internal/core/game/game.go
@@ -81,7 +81,7 @@ func initSystems(world *ecs.World) {
 func createEntities(world *ecs.World) {
 	world.EventBus.Publish(events.NewCreateShipEvent())
 	//world.EventBus.Publish(events.NewCreateAlienEvent(geometry.Vec3{X: 3, Y: 2, Z: -1}))
-	world.EventBus.Publish(events.NewMeteoriteZoneEvent)
+	world.EventBus.Publish(events.NewMeteoriteZoneEvent())
 }
 
 func timerIsOver(publisher transfer.EventPublisher) {
